3133d643274ba9be028647f2f62ac8f4a276ac3a
Barış Sokat <barissokat@outlook.com>
2022-01-11 13:25:48 +0300

    Use Context to manage the auth state

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-
-import './index.css'
+import { AuthContextProvider } from './store/auth-context'
 import App from './App'
 
+import './index.css'
+
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <AuthContextProvider>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </AuthContextProvider>,
   document.getElementById('root')
 )
