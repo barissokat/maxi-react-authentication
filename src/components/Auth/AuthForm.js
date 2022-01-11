9198c048f122c1f41e8a78a7726e6b06fbd6fd93
Barış Sokat <barissokat@outlook.com>
2022-01-11 11:28:50 +0300

    Show signup feedback to the user

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -7,6 +7,7 @@ const AuthForm = () => {
   const passwordInputRef = useRef()
 
   const [isLogin, setIsLogin] = useState(true)
+  const [isLoading, setIsLoading] = useState(false)
 
   const switchAuthModeHandler = () => {
     setIsLogin(prevState => !prevState)
@@ -19,6 +20,9 @@ const AuthForm = () => {
     const enteredPassword = passwordInputRef.current.value
 
     // optional validation
+
+    setIsLoading(true)
+
     if (isLogin) {
       // ...
     } else {
@@ -36,12 +40,16 @@ const AuthForm = () => {
           }
         }
       ).then(res => {
+        setIsLoading(false)
         if (res.ok) {
           // ...
         } else {
           return res.json().then((data) => {
-            // show an error modal
-            console.log(data)
+            let errorMessage = 'Authentication failed!'
+            // if (data && data.error && data.error.message) {
+            //   errorMessage = data.error.message
+            // }
+            alert(errorMessage)
           })
         }
       })
@@ -61,7 +69,8 @@ const AuthForm = () => {
           <input type='password' id='password' required ref={passwordInputRef} />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+          {isLoading && <p>Sending request...</p>}
           <button type='button' className={classes.toggle} onClick={switchAuthModeHandler}>
             {isLogin ? 'Create new account' : 'Login with existing account'}
           </button>
